137480f945dec5f630fa4335d2e2c64890cbee48
Kay <kayvvw@gmail.com>
2016-08-17 00:51:51 +1200

    Start develop database
    Create DishBaseHelper and DishDbSchema.

--- app/src/main/java/nz/ac/unitec/restaurantordersystem/DishLab.java
+++ app/src/main/java/nz/ac/unitec/restaurantordersystem/DishLab.java
@@ -1,27 +1,27 @@
 package nz.ac.unitec.restaurantordersystem;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import nz.ac.unitec.restaurantordersystem.database.DishBaseHelper;
 
 /**
  * Created by Kay on 27/07/2016.
  */
 public class DishLab {
-    private ArrayList<Dish> mDishes;
     private static DishLab sDishLab;
-    private Context mAppContext;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+
 
     private DishLab(Context appContext) {
-        mAppContext = appContext;
-        mDishes = new ArrayList<Dish>();
-        for (int i = 0; i < 100; i++) {
-            Dish d = new Dish();
-            d.setName("Dish #" + i);
-            d.setDescription("Description#"+i);
-            mDishes.add(d);
-        }
+        mContext = appContext.getApplicationContext();
+        mDatabase = new DishBaseHelper(mContext).getWritableDatabase();
     }
 
     public static DishLab get(Context context){
@@ -31,16 +31,13 @@ public class DishLab {
         return sDishLab;
     }
 
-    public ArrayList<Dish> getDishes(){
-        return mDishes;
+    public void addDish(Dish d){};
+
+    public List<Dish> getDishes(){
+        return new ArrayList<>();
     }
 
     public Dish getDish(UUID id){
-        for(Dish d : mDishes){
-            if(d.getId().equals(id)){
-                return d;
-            }
-        }
         return null;
     }
 }
